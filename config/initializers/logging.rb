21d1fdbc8ee6abf719fd0f9cf83c0ac4e923c2e1
Eric Garside <garside@gmail.com>
2018-05-09 03:03:04 -0400

    Logger fixes

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -2,7 +2,7 @@
 
 Rails.application.configure do
   config.lograge.enabled = true
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.formatter = Lograge::Formatters::Logstash.new(type: :stdout)
   config.lograge.custom_options = lambda do |event|
     exceptions = %w[controller action format id]
     { params: event.payload[:params].except(*exceptions) }
