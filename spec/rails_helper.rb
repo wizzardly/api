01d372af871b4ad043de7208f1941d35fb882d5d
Eric Garside <garside@gmail.com>
2018-05-08 23:41:38 -0400

    More rubocop refinement

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ end
 
 ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path("../../config/environment", __dir__)
+require File.expand_path("../../config/environment", __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
