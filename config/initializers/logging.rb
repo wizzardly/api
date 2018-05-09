ddd165be8cee8f2179ffb8250a83cc636b6f8a38
Eric Garside <garside@gmail.com>
2018-05-09 03:37:24 -0400

    Testing

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   config.lograge.base_controller_class = "ActionController::API"
   config.lograge.enabled = true
-  # config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.lograge.custom_options = lambda do |event|
     exceptions = %w[controller action format id]
     { params: event.payload[:params].except(*exceptions) }
