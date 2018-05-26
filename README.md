c405f4f4e71c34e909116774fe30d78a2221238d
Eric Garside <garside@gmail.com>
2018-05-26 11:18:24 -0400

    More cleanup & README

--- README.md
+++ README.md
@@ -5,3 +5,29 @@
 [![Test Coverage](https://api.codeclimate.com/v1/badges/3817b057abd79f487c3d/test_coverage)](https://codeclimate.com/github/wizzardly/wizzardly-api/test_coverage)
 
 An open source card game server.
+
+# Installation
+
+1. Clone this repository, then:
+
+```bash
+cd /path/to/wizzardly-api
+bundle install
+rake db:create db:migrate
+```
+
+1. Install and configure [puma-dev](https://github.com/puma/puma-dev), then:
+
+```bash
+cd ~/.puma-dev
+ln -s /path/to/wizzardly-api api.wizzardly
+```
+
+1. Visit `https://api.wizzardly.dev`
+1. Success!
+
+# Testing
+
+```bash
+rake
+```
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp", "caching-dev.txt").exist?
+  if Settings.application.perform_caching
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
@@ -34,6 +34,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+
+  # Configure the mailer
+  config.action_mailer.default_url_options = { host: Settings.application.host_name }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
