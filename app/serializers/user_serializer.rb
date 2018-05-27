3e24016226efaabc35dbf99ba28ab1c3e7af5daf
Eric Garside <garside@gmail.com>
2018-05-27 16:14:59 -0400

    UserSerializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at      :datetime         not null
+#  email           :string           not null
+#  first_name      :string           not null
+#  id              :bigint(8)        not null, primary key
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
+class UserSerializer < ApplicationSerializer
+  attributes :name, :first_name, :last_name, :initials, :email
+end
