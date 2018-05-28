7c5a83e54aac34f995405984f8a1dec6ef9efe26
Eric Garside <garside@gmail.com>
2018-05-27 20:13:16 -0400

    Removing default

--- lib/generators/rspec/logger/templates/logger_spec.rb
+++ lib/generators/rspec/logger/templates/logger_spec.rb
@@ -4,5 +4,4 @@ require "rails_helper"
 
 RSpec.describe <%= class_name %>Logger, type: :logger do
   it { expect(described_class <= ApplicationLogger).to be_truthy }
-  it "is pending more examples"
 end
