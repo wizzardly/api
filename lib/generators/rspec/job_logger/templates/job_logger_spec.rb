35a5a8bf6a9c59dc479b7e775cb2086b83c9793c
Eric Garside <garside@gmail.com>
2018-05-27 20:06:23 -0400

    JobLogger & RSpec

---
+++ lib/generators/rspec/job_logger/templates/job_logger_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe <%= class_name %>JobLogger, type: :logger do
+  it { expect(described_class <= ApplicationCacheLogger).to be_truthy }
+
+
+end
