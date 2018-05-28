52f3abb1b745c42f0ced51a319d9af032bb47394
Eric Garside <garside@gmail.com>
2018-05-27 20:12:00 -0400

    Overwrite default job generator

--- lib/generators/job/job/job_generator.rb
+++ lib/generators/job/job/job_generator.rb
@@ -4,8 +4,8 @@ require "rails/generators/job/job_generator"
 
 module Job
   class JobGenerator < Rails::Generators::JobGenerator
-    class_option :logger, type: :boolean, default: true
+    class_option :loggers, type: :boolean, default: true
 
-    hook_for :logger
+    hook_for :loggers
   end
 end
