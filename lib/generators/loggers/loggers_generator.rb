260d1dcb07b26198a8b3538357403a628a1ea1a8
Eric Garside <garside@gmail.com>
2018-05-27 20:00:04 -0400

    Renaming

--- lib/generators/loggers/loggers_generator.rb
+++ lib/generators/loggers/loggers_generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class LoggerGenerator < Rails::Generators::NamedBase
+class LoggersGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
   check_class_collision suffix: "Logger"
