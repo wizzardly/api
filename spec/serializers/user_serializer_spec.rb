9654ce5b94b195a01ff20fb2b9fdeed8e02b2bd3
Eric Garside <garside@gmail.com>
2018-06-06 20:39:38 -0400

    Fixing serializer spec

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at      :datetime         not null
+#  email           :string           not null
+#  first_name      :string           not null
+#  id              :bigint(8)        not null, primary key
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
+
 require "rails_helper"
 
 RSpec.describe UserSerializer, type: :serializer do
