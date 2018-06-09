b4bf9376fc3d388233da51a2980a77d6819956d6
Eric Garside <garside@gmail.com>
2018-06-08 20:46:50 -0400

    Logging: A Solved Problem

--- app/jobs/sentry_job.rb
+++ app/jobs/sentry_job.rb
@@ -6,7 +6,7 @@ class SentryJob < ApplicationJob
   rescue_from ActiveJob::DeserializationError do |deserialization_error|
     # If not rescued, this error causes an infinite loop.
     ActiveSupport::Notifications.instrument(
-      "deserialization_error.sentry_job",
+      "deserialization_error.sentry_job.error",
       event_hash: arguments[0],
       deserialization_error: deserialization_error,
     )
@@ -14,11 +14,11 @@ class SentryJob < ApplicationJob
 
   def perform(event_hash)
     unless Settings.sentry.enable
-      ActiveSupport::Notifications.instrument "sentry_disabled.sentry_job", event_hash: event_hash
+      ActiveSupport::Notifications.instrument "sentry_disabled.sentry_job.debug", event_hash: event_hash
       return
     end
 
     Raven.send_event(event_hash)
-    ActiveSupport::Notifications.instrument "sent_to_sentry.sentry_job"
+    ActiveSupport::Notifications.instrument "sent_to_sentry.sentry_job.info"
   end
 end
--- config/initializers/log_subscriptions.rb
+++ config/initializers/log_subscriptions.rb
@@ -1,4 +1,25 @@
-# frozen_string_literal: true
+class ApplicationLogger
+  def call(name, started, finished, _unique_id, payload)
+    Rails.logger.public_send(criticality) do
+      { event: name.chomp(".#{criticality}"), started: started, finished: finished }.compact.merge(payload)
+    end
+  end
 
-SentryJobLogger.attach_to :sentry_job
-ApplicationCacheLogger.attach_to :application_cache
+  private
+
+  def criticality
+    @criticality ||= self.class.name.chomp("Logger").downcase.to_sym
+  end
+end
+
+class FatalLogger < ApplicationLogger; end
+class ErrorLogger < ApplicationLogger; end
+class WarnLogger < ApplicationLogger; end
+class InfoLogger < ApplicationLogger; end
+class DebugLogger < ApplicationLogger; end
+
+ActiveSupport::Notifications.subscribe(%r{\.fatal$}, FatalLogger.new)
+ActiveSupport::Notifications.subscribe(%r{\.error$}, ErrorLogger.new)
+ActiveSupport::Notifications.subscribe(%r{\.warn$}, WarnLogger.new)
+ActiveSupport::Notifications.subscribe(%r{\.info$}, InfoLogger.new)
+ActiveSupport::Notifications.subscribe(%r{\.debug$}, DebugLogger.new)
